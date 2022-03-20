887f44ea97270d6deccd9fb7a1996f8f417124f6
eomgerm <fishbread00@gmail.com>
2022-03-20 09:55:39 +0000

    initializing

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import ServerIO from "socket.io";
 
 const app = express();
 
@@ -10,65 +9,8 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log("Listening on 'http://localhost:3000'");
-
 const httpServer = http.createServer(app);
-const ioServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
+const ioServer = ServerIO(httpServer);
 
-instrument(ioServer, {
-  auth: false,
-});
-
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = ioServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-
-  return publicRooms;
-};
-
-const countUsers = (roomName) => {
-  return ioServer.sockets.adapter.rooms.get(roomName)?.size;
-};
-
-ioServer.on("connection", (socket) => {
-  socket["nickname"] = "";
-
-  socket.on("enter_room", (roomName, nickname, done) => {
-    socket["nickname"] = nickname;
-    socket.join(roomName);
-    done(countUsers(roomName));
-    socket.to(roomName).emit("welcome", socket.nickname, countUsers(roomName));
-    ioServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countUsers(room))
-    );
-  });
-
-  socket.on("disconnect", () => {
-    ioServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (message, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
-    done();
-  });
-});
-
+const handleListen = () => console.log("Listening on 'http://localhost:3000'");
 httpServer.listen(3000, handleListen);
